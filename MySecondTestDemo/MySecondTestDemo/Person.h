378274d94c2d83b3aff703cb3b713e3476ecee2b
薛明星 <xuemingxingxmx@sina.cn>
2016-12-20 23:49:39 +0800

    添加了一个新的类person，声明和实现了run方法

---
+++ MySecondTestDemo/MySecondTestDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MySecondTestDemo
+//
+//  Created by 薛明星 on 16/12/20.
+//  Copyright © 2016年 xuemingxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+- (void)run;
+@end
---
+++ MySecondTestDemo/MySecondTestDemo/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  MySecondTestDemo
+//
+//  Created by 薛明星 on 16/12/20.
+//  Copyright © 2016年 xuemingxing. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)run
+{
+    NSLog(@"人正在跑");
+}
+
+@end
